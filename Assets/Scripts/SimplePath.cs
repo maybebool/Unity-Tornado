2d12936765c49a168324b23ab2b8a9b065efe44f
Roman Aebi <unity2.sae@gmail.com>
2023-03-26 18:55:55 +0200

    SimplePath for Tornado

---
+++ Assets/Scripts/SimplePath.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SimplePath : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed = 2f;
+    private int _waypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[_waypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+            waypoints[_waypointIndex].transform.position, speed * Time.deltaTime);
+
+        if (transform.position == waypoints[_waypointIndex].transform.position)
+        {
+            _waypointIndex += 1;
+        }
+
+        if (_waypointIndex == waypoints.Length)
+        {
+            _waypointIndex = 0;
+        }
+    }
+}
