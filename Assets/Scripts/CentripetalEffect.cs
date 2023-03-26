bbc23948424455e0e7a493ffe03194c8a2c49aba
Roman Aebi <unity2.sae@gmail.com>
2023-03-26 18:49:35 +0200

    BezierCurve and Commenting

---
+++ Assets/Scripts/CentripetalEffect.cs
@@ -0,0 +1,16 @@
+
+public static class CentripetalEffect
+{
+    // scalarBetweenAB = scalarAB = (fluid value based on scalar)
+    // posPointA = a   posPointB = b   posPointC = c  posPointD = d
+    // to inverse the linear interpolation we need negative values for (b - a) and (d - c)
+    // possible is also a positive value for (b - a) if a is higher then max Scalar value of AB
+        
+    // we need a positive return overall therefore the formula must be either
+    // (+) + ((-) / (+)) * (-) || (+) + ((+) / (-)) * (-)
+        
+    // Bezier Curve explaining of a + (b - a) * t
+    public static float Swirl(this float scalarAB, float a, float b, float c, float d) {
+        return c + ((scalarAB - a) / (b - a)) * (d - c);
+    }
+}
