30837b70fe1b994c9c96d68d8a89df49e4b8f1ba
Roman Aebi <unity2.sae@gmail.com>
2023-04-30 00:16:20 +0200

    on trigger change

--- Assets/Destroyer.cs
+++ Assets/Destroyer.cs
@@ -1,9 +1,9 @@
+using System;
 using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        Destroy(collision.gameObject);
+    private void OnTriggerEnter(Collider other) {
+        Destroy(other.gameObject);
     }
 }
