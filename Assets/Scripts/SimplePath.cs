c811964b614d05f8d9289b6c2d464c0414e22982
Roman Aebi <unity2.sae@gmail.com>
2023-05-03 22:09:05 +0200

    cleaning 2

--- Assets/Scripts/SimplePath.cs
+++ Assets/Scripts/SimplePath.cs
@@ -4,30 +4,25 @@ public class SimplePath : MonoBehaviour
 {
     [SerializeField] private Transform[] waypoints;
     [SerializeField] private float speed = 2f;
-    private int _waypointIndex = 0;
+    private int _waypointIndex;
 
-    private void Start()
-    {
+    private void Start() {
         transform.position = waypoints[_waypointIndex].transform.position;
     }
 
-    private void Update()
-    {
+    private void Update() {
         Patrol();
     }
 
-    private void Patrol()
-    {
+    private void Patrol() {
         transform.position = Vector3.MoveTowards(transform.position,
             waypoints[_waypointIndex].transform.position, speed * Time.deltaTime);
 
-        if (transform.position == waypoints[_waypointIndex].transform.position)
-        {
+        if (transform.position == waypoints[_waypointIndex].transform.position) {
             _waypointIndex += 1;
         }
 
-        if (_waypointIndex == waypoints.Length)
-        {
+        if (_waypointIndex == waypoints.Length) {
             _waypointIndex = 0;
         }
     }
