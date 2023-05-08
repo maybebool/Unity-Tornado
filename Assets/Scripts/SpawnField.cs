39a88ec71d57236b836ca3ce6ef3b89e74cb5815
Roman Aebi <unity2.sae@gmail.com>
2023-05-08 21:29:55 +0200

    try to fix Addressable release

--- Assets/Scripts/SpawnField.cs
+++ Assets/Scripts/SpawnField.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 
 public class SpawnField : Singleton<SpawnField>
@@ -13,31 +17,47 @@ public class SpawnField : Singleton<SpawnField>
     [SerializeField] private Vector3 center;
     [SerializeField] private AudioSource sound;
     private List<AssetReferenceGameObject> PrefabList { get; set; }
-    
-    //private Dictionary<AssetReferenceGameObject, List<GameObject>> PrefabDictionary { get; set; }
+
+    public List<AssetLabelReference> assetLabelReferences = new();
+
+    private AsyncOperationHandle<IList<GameObject>> _loadHandle;
     private GameObject Prefab { get; set; }
+    //private Dictionary<AssetReferenceGameObject, List<GameObject>> PrefabDictionary { get; set; }
 
     private void Start() {
-        PrefabList = new List<AssetReferenceGameObject>();
+        //assetLabelReferences = new List<AssetLabelReference>();
     }
 
+    // TODO releasing multiple Objects is still a problem
     private void DoVoxelGrid() {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 for (int z = 0; z < length; z++) {
                     var pos =  center + new Vector3( x, y, z ) * threshold;
-                    assetPrefab.InstantiateAsync(pos,Quaternion.identity).Completed += (asyncOperation) => Prefab = asyncOperation.Result;
-                    // var gridPrefab =  Instantiate(prefab, pos, Quaternion.identity);
-                    //PrefabList.Add(Prefab);
-                    
+
+                    _loadHandle = Addressables.LoadAssetsAsync<GameObject>(assetLabelReferences, addressable => {
+                        Instantiate(addressable, pos, Quaternion.identity);
+                    }, Addressables.MergeMode.Union, true);
                 }
             }
         }
     }
+                    //assetPrefab.InstantiateAsync(pos,Quaternion.identity).Completed += (asyncOperation) => Prefab = asyncOperation.Result;
+                    // var gridPrefab =  Instantiate(prefab, pos, Quaternion.identity);
+                    //PrefabList.Add(Prefab);
 
     private void OnMouseDown() {
         DoVoxelGrid();
         sound.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A was pressed");
+            DeleteAll();
+        }
     }
 
     public void DeleteAll() {
@@ -46,6 +66,9 @@ public class SpawnField : Singleton<SpawnField>
         //     p.ReleaseInstance(Prefab);
         //
         // }
-        assetPrefab.ReleaseInstance(Prefab);
+        //assetPrefab.ReleaseInstance(Prefab);
+        Addressables.Release(_loadHandle);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 }
