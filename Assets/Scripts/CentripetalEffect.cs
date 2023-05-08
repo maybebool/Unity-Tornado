0b6d4e902553e3dd1fa653f06e74a51fadff0697
Roman Aebi <unity2.sae@gmail.com>
2023-05-08 11:06:21 +0200

    Prefab naming

--- Assets/Scripts/CentripetalEffect.cs
+++ Assets/Scripts/CentripetalEffect.cs
@@ -2,15 +2,15 @@
 public static class CentripetalEffect
 {
     // scalarBetweenAB = scalarAB = (fluid value based on scalar)
-    // posPointA = a   posPointB = b   posPointC = c  posPointD = d
-    // to inverse the linear interpolation we need negative values for (b - a) and (d - c)
+    // posPointA = a   posPointB = b   ToA = c  ToB = d
+    // to inverse the linear interpolation we need negative values for (d - c)
     // possible is also a positive value for (b - a) if a is higher then max Scalar value of AB
         
     // we need a positive return overall therefore the formula must be either
     // (+) + ((-) / (+)) * (-) || (+) + ((+) / (-)) * (-)
         
     // Bezier Curve explaining of a + (b - a) * t
-    public static float Swirl(this float scalarAB, float a, float b, float c, float d) {
-        return (scalarAB - a) / (b - a) * (d - c) + c;
+    public static float TornadoRotation(this float scalarAb, float a, float b, float c, float d) {
+        return (scalarAb - a) / (b - a) * (d - c) + c;
     }
 }
--- Assets/Scripts/Effector.cs
+++ Assets/Scripts/Effector.cs
@@ -1,45 +1,41 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Effector : MonoBehaviour
 {
     private const float PosPointA = 0;
     private const float PosPointB = 10;
+    
+    // a positive value of (toA - toB) would lead to a negative return in the Tornado effect.
+    private const int ToA = 1;
+    private const int ToB = 0;
 
 
-    [Header("Tornado Force")] [Tooltip("The power how strong an object will be hold into the tornado")] [SerializeField]
-    private float centripetalForce = 10;
+    [Tooltip("The power how strong an object will be hold into the tornado")] 
+    [SerializeField] private float centripetalForce = 10;
+    
+    
+    [Tooltip("Counter force to centripetal Force")]
+    [SerializeField] private float push = 3;
+    [SerializeField] private float forceMultiplier = 10f;
+    
+    
+    [Tooltip("Very fragile Value.")] 
+    [SerializeField] private float reverseIndicationValue = 0.8f;
 
-    [FormerlySerializedAs("acceleration")]
-    [Tooltip("The power how fast an object will be sucked into the tornado")]
-    [SerializeField]
-    private float push = 3;
-
-    [Tooltip("Overall multiplier. Higher value means bigger tornado swirl diameter")] [SerializeField]
-    private float forceMultiplier = 10f;
-
-    [Header("Tornado Swirl Behaviour")] [Tooltip("Very fragile Value.")] [SerializeField]
-    private float reverseIndicationValue = 0.8f;
-
-
-    // a positive value of (d-c) would lead to a negative return in the Swirl effect.
-    // tofA must be bigger then tofB because we need a negative value for t
-    private int tofA = 1;
-    private int tofB = 0;
-
+    
     private float _thresholdDistance;
     private float _scalarBetweenAb;
     private Vector3 _vectorAToB;
     private Vector3 _direction;
 
-    private void Suction(Collider col) {
-        _vectorAToB = transform.position - col.transform.position;
+    private void TornadoPhysics(Collider objCol) {
+        _vectorAToB = transform.position - objCol.transform.position;
         _scalarBetweenAb = _vectorAToB.magnitude;
         _direction = _vectorAToB / _scalarBetweenAb;
 
         // takes the scalar of AB as input and generate a quasi linear interpolation between two points
         // remap distance will reverse after max reach
-        _thresholdDistance = _scalarBetweenAb.Swirl(PosPointA, PosPointB, tofA, tofB);
+        _thresholdDistance = _scalarBetweenAb.TornadoRotation(PosPointA, PosPointB, ToA, ToB);
 
         // if the remapDistance is lower then the given value it reserve the direction by acceleration and gives the needed tornado effect
         // as the objects starts with acceleration then gets moved from A to B by the vector direction. If max is reached direction 
@@ -49,13 +45,13 @@ public class Effector : MonoBehaviour
             _thresholdDistance = push;
         }
 
-        if (col.attachedRigidbody != null)
-            col.attachedRigidbody.AddForce(_direction * centripetalForce * forceMultiplier * _thresholdDistance);
+        if (objCol.attachedRigidbody != null)
+            objCol.attachedRigidbody.AddForce(_direction * centripetalForce * forceMultiplier * _thresholdDistance);
     }
 
     private void OnTriggerStay(Collider other) {
-        if (other.CompareTag("Spinable")) {
-            Suction(other);
+        if (other.CompareTag("Object")) {
+            TornadoPhysics(other);
         }
     }
 }
