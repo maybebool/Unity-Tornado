81d9dbe32ab1e2e5e42017076de650ba8e47d1a3
Roman Aebi <unity2.sae@gmail.com>
2023-05-09 16:35:44 +0200

    try to fix addressables

--- Assets/Scripts/SpawnAddressables.cs
+++ Assets/Scripts/SpawnAddressables.cs
@@ -1,10 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceLocations;
 
 public class SpawnAddressables : MonoBehaviour
 {
-    private void LoadVoxelGrid()
+    public static async Task InitByNameOrLabel<T>(string assetNameOrLabel, List<T> createdObjs)
+        where T : Object
     {
+        var locations = await Addressables.LoadResourceLocationsAsync(assetNameOrLabel).Task;
         
+        await CreateAssetsThenUpdateCollection(locations, createdObjs);
     }
+    
+    public static async Task IniByLoadedAddress<T>(IList<IResourceLocation> loadedLocations, List<T> createdObjs)
+        where T : Object
+    {
+        await CreateAssetsThenUpdateCollection(loadedLocations, createdObjs);
+    }
+
+    private static async Task CreateAssetsThenUpdateCollection<T>(IList<IResourceLocation> locations, List<T> createdObjs)
+        where T: Object
+    {
+        foreach (var location in locations)
+            createdObjs.Add(await Addressables.InstantiateAsync(location).Task as T);
+    }
+
 }
--- Assets/Scripts/SpawnField.cs
+++ Assets/Scripts/SpawnField.cs
@@ -1,22 +1,22 @@
-using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
+
 
 
 public class SpawnField : Singleton<SpawnField>
 {
-    private List<AsyncOperationHandle<IList<GameObject>>> _assetPrefab;
+    private List<GameObject> AssetPrefabs { get; set; }
+    private List<GameObject> PrefabList { get; set; }
+    [SerializeField] private AssetReferenceGameObject assetPrefab;
     [SerializeField] private int width = 10;
     [SerializeField] private int length = 10;
     [SerializeField] private int height = 10;
     [SerializeField] private float threshold = 0.2f;
     [SerializeField] private Vector3 center;
     [SerializeField] private AudioSource sound;
-    private List<AssetReferenceGameObject> PrefabList { get; set; }
 
     public List<AssetLabelReference> assetLabelReferences = new();
 
@@ -24,27 +24,35 @@ public class SpawnField : Singleton<SpawnField>
     private GameObject Prefab { get; set; }
     //private Dictionary<AssetReferenceGameObject, List<GameObject>> PrefabDictionary { get; set; }
 
-    private void Start()
-    {
-        _assetPrefab = new List<AsyncOperationHandle<IList<GameObject>>>();
+    [SerializeField] private string label;
+    private List<GameObject> Assets { get; } = new();
+
+    private void Awake() {
+        //AssetPrefabs = new List<GameObject>();
     }
 
     // TODO releasing multiple Objects is still a problem
-    private void DoVoxelGrid() {
+    private async Task DoVoxelGrid() {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 for (int z = 0; z < length; z++) {
                     var pos =  center + new Vector3( x, y, z ) * threshold;
+                    await SpawnAddressables.InitByNameOrLabel(label, Assets);
+                    foreach (var asset in Assets)
+                    {
+                        
+                    }
+                    // assetPrefab.InstantiateAsync(pos,Quaternion.identity).Completed += (asyncOperation) => Prefab = asyncOperation.Result;
+                    // AssetPrefabs.Add(Prefab);
 
-                    _loadHandle = Addressables.LoadAssetsAsync<GameObject>(assetLabelReferences, addressable => {
-                        Instantiate(addressable, pos, Quaternion.identity);
-                        _assetPrefab.Add(_loadHandle);
-                    }, Addressables.MergeMode.Union, true);
+                    // _loadHandle = Addressables.LoadAssetsAsync<GameObject>(assetLabelReferences, addressable => {
+                    //     Instantiate(addressable, pos, Quaternion.identity);
+                    //     _assetPrefab.Add(addressable);
+                    // }, Addressables.MergeMode.Union, true);
                 }
             }
         }
     }
-                    //assetPrefab.InstantiateAsync(pos,Quaternion.identity).Completed += (asyncOperation) => Prefab = asyncOperation.Result;
                     // var gridPrefab =  Instantiate(prefab, pos, Quaternion.identity);
                     //PrefabList.Add(Prefab);
 
@@ -56,22 +64,31 @@ public class SpawnField : Singleton<SpawnField>
     private void Update() {
         if (Input.GetKeyDown(KeyCode.A)) {
             Debug.Log("A was pressed");
-            DeleteAll();
+            //DeleteAll();
+            CleanUpFinishedAssets(Assets[0]);
         }
     }
+    
+    public async Task CleanUpFinishedAssets(Object obj)
+    {
+        Addressables.Release(obj);
+    }
 
-    public void DeleteAll() {
+    public void DeleteAll()
+    {
         // foreach (var p in PrefabList) {
         //     Debug.Log("List is " +PrefabList + "long");
         //     p.ReleaseInstance(Prefab);
         //
         // }
         //assetPrefab.ReleaseInstance(Prefab);
-        foreach (var p in _assetPrefab) {
-            Addressables.Release(p);
+        // foreach (var p in _assetPrefab) {
+        //     Addressables.Release(p);
+        // }
+        foreach (GameObject t in AssetPrefabs) {
+            assetPrefab.ReleaseInstance(t);
         }
         //Addressables.ReleaseInstance(_loadHandle);
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
     }
 }
